636477a6cdfe9cd3f591a78a18d186c7a316448a
iPolishchuk <79876555+polarspb@users.noreply.github.com>
2021-06-21 23:31:15 +0300

    Update entrypoint.sh

--- .github/actions/action_1.2/entrypoint.sh
+++ .github/actions/action_1.2/entrypoint.sh
@@ -12,6 +12,6 @@ pwd
 echo "ls -lah /github/workspace"
 ls -lah /github/workspace
 
-sudo chmod 655 log.md
+chmod 777 log.md
 
-sudo echo "Text1: ${{ github.event.inputs.text1 }}" >> log.md
+echo "Text1: ${{ github.event.inputs.text1 }}" >> log.md
