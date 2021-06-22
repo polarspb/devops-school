87ac8abe2c50e9d2c72592d5361fca85663976fb
iPolishchuk <79876555+polarspb@users.noreply.github.com>
2021-06-22 15:45:53 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,8 +3,10 @@ from datetime import datetime
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+text = github.event.inputs.text1
 
 with open('log.md', 'w') as f:
     f.write(f'# {timestamp}')
+    f.write(f'# {text}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
